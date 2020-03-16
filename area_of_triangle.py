59738628ac335c75634dedcf4b9af05e4a14e679
ashukumar2001 <62238292+ashukumar2001@users.noreply.github.com>
2020-03-16 14:41:32 +0530

    Create area_of_triangle.py

---
+++ area_of_triangle.py
@@ -0,0 +1,4 @@
+len_base = float(input("Input the length of base: "))
+len_hypo = float(input("Input the lenght of hypotaneous: "))
+are_of_triangle = (len_base * len_hypo) * .5
+print("The area of triangle is:", are_of_triangle)
